549816b541907f9e1893285080111a2915e15a83
JobLopez <jobylopez937@gmail.com>
2026-04-20 14:05:38 -0700

    gradle update

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+/**
+ * @author Freddy Sotelo
+ * created: 4/20/26
+ * @since Assignment: Default (Template) Project
+ **///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage){
+//        Label label = new Label("Hello Jobless Otters");
+//        Scene scene = new Scene(label, 400, 200);
+//        stage.setScene(scene);
+        stage.setTitle("Team Jobless Otters");
+        stage.show();
+
+    }
+}
