649106777d331285cbb382751d0c1b75db912821
Freddy Sotelo <fsotelo@csumb.edu>
2026-04-26 15:02:48 -0700

    Created Enum Test

--- src/main/java/SceneFactory.java
+++ src/main/java/SceneFactory.java
@@ -26,7 +26,7 @@ public class SceneFactory {
             case LOGIN -> buildLoginScene(stage);
             case SIGNUP -> buildSIGNUPScene(stage);
             case MAIN -> buildMAINScene(stage);
-            case Profile -> buildProfilePage(stage);
+            case PROFILE -> buildProfilePage(stage);
         };
     }
 
@@ -119,7 +119,7 @@ public class SceneFactory {
 
         profileBtn.setMaxWidth(Double.MAX_VALUE);
         profileBtn.setOnAction(e ->
-                stage.setScene(create(SceneType.Profile, stage)));
+                stage.setScene(create(SceneType.PROFILE, stage)));
 
         settingsBtn.setMaxWidth(Double.MAX_VALUE);
 
